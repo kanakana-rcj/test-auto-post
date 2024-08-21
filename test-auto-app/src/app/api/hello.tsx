b252f57e2a695dc696840757aea0022e17b2e441
kanakana-rcj <nakagawa.dadada5@gmail.com>
2024-08-21 19:47:10 +0900

    add auto-post-function

--- test-auto-app/src/app/api/hello.tsx
+++ test-auto-app/src/app/api/hello.tsx
@@ -4,11 +4,9 @@ import { firestore } from "@/firebase";
 
 export default async function () {
   try {
-    const newCityRef = doc(collection(firestore, "test"));  
-    const data = {text: "hello2"};
-    await setDoc(newCityRef, data);
-    const data2 = {text: "hello3"};
-    await addDoc(collection(firestore, "test"), data2);
+    const doc = await addDoc(collection(firestore, "test"), {
+      text: "hello2"
+    }); 
   } catch (error) {
     console.log(error);
   }
