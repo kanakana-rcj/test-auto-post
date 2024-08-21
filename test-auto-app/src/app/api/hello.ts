accb66cc8a76917e87e84b17fb4cc67a288d1ec6
kanakana-rcj <nakagawa.dadada5@gmail.com>
2024-08-21 19:30:43 +0900

    add auto-post-function

--- test-auto-app/src/app/api/hello.ts
+++ test-auto-app/src/app/api/hello.ts
@@ -1,25 +1,17 @@
-'use client'
-
 import { VercelResponse } from "@vercel/node";
 import { addDoc, setDoc, doc, collection } from "firebase/firestore";
 import { firestore } from "@/firebase";
-import { useEffect } from "react";
 
-export default function ( res: VercelResponse ) {
-  res.send(`Hello!`);
-  useEffect(() => {
-
-    const addingDoc = async () => {
-      const newCityRef = doc(collection(firestore, "test"));  
-      const data = {text: "hello2"};
-      await setDoc(newCityRef, data);
-      const data2 = {text: "hello3"};
-      await addDoc(collection(firestore, "test"), data2);
-    };
-
-    addingDoc();
-  }, []);
-
+export default async function () {
+  try {
+    const newCityRef = doc(collection(firestore, "test"));  
+    const data = {text: "hello2"};
+    await setDoc(newCityRef, data);
+    const data2 = {text: "hello3"};
+    await addDoc(collection(firestore, "test"), data2);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export const runtime = 'nodejs';
