fa1fd116d0953fea75e41ab97cfd13945e9e2de6
kanakana-rcj <nakagawa.dadada5@gmail.com>
2024-08-22 14:03:32 +0900

    add auto-post-function

--- test-auto-app/src/app/api/hello.tsx
+++ test-auto-app/src/app/api/hello.tsx
@@ -1,17 +1,24 @@
 import { addDoc, collection } from "firebase/firestore";
 import { firestore } from "@/firebase";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export const runtime = 'nodejs';
+// export const runtime = 'nodejs';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
     const doc = await addDoc(collection(firestore, "test"), {
       text: "hello2"
     }); 
     console.log("succeded");
-    res.status(200).json({ message: 'Task executed' });
-  } catch (error) {
+    res.status(200).json({
+      message: 'hello2',
+      cookies: req.cookies,
+    })
+  } catch (error: any) {
     console.log(error);
+    res.status(200).json({
+      message: 'hello2',
+      cookies: req.cookies,
+    })
   }
 }
